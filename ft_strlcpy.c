0b32b0992ba186001ae10ef0d794c993c04a1da8
Pouya Ghajardavalou <ghajar.pouya@gmail.com>
2024-03-17 21:56:15 +0100

    file added

---
+++ ft_strlcpy.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	count;
+
+	i = 0;
+	count = 0;
+	while (src[count] != '\0')
+	{
+		count++;
+	}
+	if (size < 1)
+		return (count);
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (count);
+}
